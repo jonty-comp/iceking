f188e530f622e8f74fa27cbcd5d90f254af4996f
jonty-comp <jontysewell@gmail.com>
2013-05-22 23:42:16 +0100

    we might not want to run as a daemon sometimes

--- main.py
+++ main.py
@@ -119,10 +119,12 @@ if __name__ == "__main__":
 				daemon.stop()
 			elif 'restart' == sys.argv[1]:
 				daemon.restart()
+			elif 'no-daemon':
+				daemon.run()
 			else:
 				print("Unknown command")
 				sys.exit(2)
 			sys.exit(0)
 	else:
-		print("usage: %s start|stop|restart" % sys.argv[0])
+		print("usage: %s start|stop|restart|no-daemon" % sys.argv[0])
 		sys.exit(2)
