b3ed6b9e40e4be8346969172d991656c9fa2d7ad
jonty-comp <jontysewell@gmail.com>
2013-05-22 23:38:45 +0100

    Classificated and daemonized the whole thing

--- main.py
+++ main.py
@@ -1,107 +1,128 @@
 #!/usr/bin/python3
-import time, os, re, string, psycopg2
+import sys, time, os, re, string, psycopg2
 from collections import defaultdict, namedtuple
+from daemon import Daemon
 
-filename = '/var/log/icecast2/access.log'
+class IceKing(Daemon):
 
-format = re.compile( 
-	r"(?P<host>[\d\.]+)\s" 
-	r"(?P<identity>\S*)\s" 
-	r"(?P<user>\S*)\s"
-	r"\[(?P<time>.*?)\]\s"
-	r'"(?P<request>.*?)"\s'
-	r"(?P<status>\d+)\s"
-	r"(?P<bytes>\S*)\s"
-	r'"(?P<referer>.*?)"\s'
-	r'"(?P<user_agent>.*?)"\s*' 
-)
+	filename = '/var/log/icecast2/access.log'
 
-LogItem = namedtuple('LogItem',
-	['host', 'identity', 'user', 'time', 'request',
-	'status', 'bytes', 'referer', 'user_agent'])
+	format = re.compile( 
+		r"(?P<host>[\d\.]+)\s" 
+		r"(?P<identity>\S*)\s" 
+		r"(?P<user>\S*)\s"
+		r"\[(?P<time>.*?)\]\s"
+		r'"(?P<request>.*?)"\s'
+		r"(?P<status>\d+)\s"
+		r"(?P<bytes>\S*)\s"
+		r'"(?P<referer>.*?)"\s'
+		r'"(?P<user_agent>.*?)"\s*' 
+	)
 
-def parse(line):
-	match = format.match(line)
-	if match:
-		return LogItem( **match.groupdict() )
+	LogItem = namedtuple('LogItem',
+		['host', 'identity', 'user', 'time', 'request',
+		'status', 'bytes', 'referer', 'user_agent'])
 
-def connect():
-	conn = psycopg2.connect(host='dbserver', database='icecast_logs', user='icecast_user', password='icecast_password')
-	return conn
+	def parse(self, line):
+		match = self.format.match(line)
+		if match:
+			return self.LogItem( **match.groupdict() )
 
-def get_mount_point(mount_point):
-	url = mount_point.split(' ')[1][1:]
-	cur.execute('SELECT id FROM mount_points WHERE (mount_point = %s) AND ((date_added <= now() AND date_retired >= now()) OR (date_added <= now() AND date_retired is NULL))', [url])
-	if cur.rowcount == 0:
-		return None
-	else:
-		id = cur.fetchone()[0]
-		return id
+	def connect(self):
+		conn = psycopg2.connect(host='dbserver', database='icecast_logs', user='icecast_user', password='icecast_password')
+		return conn
 
-def get_or_add_ua(ua):
-	cur.execute('SELECT id FROM user_agents WHERE ua_string = %s', [ua])
-	if cur.rowcount == 0:
-		cur.execute('INSERT INTO user_agents (ua_string) VALUES (%s) RETURNING id', [ua])
-		id = cur.fetchone()[0]
+	def get_mount_point(self, mount_point):
+		url = mount_point.split(' ')[1][1:]
+		self.cur.execute('SELECT id FROM mount_points WHERE (mount_point = %s) AND ((date_added <= now() AND date_retired >= now()) OR (date_added <= now() AND date_retired is NULL))', [url])
+		if self.cur.rowcount == 0:
+			return None
+		else:
+			id = self.cur.fetchone()[0]
+			return id
+
+	def get_or_add_ua(self, ua):
+		self.cur.execute('SELECT id FROM user_agents WHERE ua_string = %s', [ua])
+		if self.cur.rowcount == 0:
+			self.cur.execute('INSERT INTO user_agents (ua_string) VALUES (%s) RETURNING id', [ua])
+			id = self.cur.fetchone()[0]
+			if id:
+				self.db.commit()
+				return id
+			else:
+				raise Exception('cannot add user-agent: %s' % ua)
+		else:
+			id = self.cur.fetchone()[0]
+			return id
+
+	def get_or_add_ref(self, url):
+		self.cur.execute('SELECT id FROM referers WHERE url = %s', [url])
+		if self.cur.rowcount == 0:
+			self.cur.execute('INSERT INTO referers (url) VALUES (%s) RETURNING id', [url])
+			id = self.cur.fetchone()[0]
+			if id:
+				self.db.commit()
+				return id
+			else:
+				raise Exception('cannot add referer: %s' % url)
+		else:
+			id = self.cur.fetchone()[0]
+			return id
+
+	def log(self, ip, mount, ref, ua, bytes):
+		self.cur.execute('INSERT INTO log (ip, mount_point_id, referer_id, user_agent_id, bytes) VALUES (%s, %s, %s, %s, %s) RETURNING id', [ip, mount, ref, ua, bytes])
+		id = self.cur.fetchone()[0]
 		if id:
-			db.commit()
+			self.db.commit()
 			return id
 		else:
-			raise Exception('cannot add user-agent: %s' % ua)
+			raise Exception('cannot log row: %s %s %s %s %s' % (id, mount, ref, ua, bytes))
+
+
+	def open_file(self, filename):
+		file = open(filename, 'r')
+
+		st_results = os.stat(filename)
+		st_size = st_results[6]
+		file.seek(st_size)
+
+		return file
+
+	def read_loop(self, file):
+		while 1:
+			where = file.tell()
+			line = file.readline()
+			if not line:
+				time.sleep(0.5)
+				file.seek(where)
+			else:
+				data = self.parse(line)
+				print(line)
+				mount = self.get_mount_point(data.request)
+				if mount is not None:
+					ua = self.get_or_add_ua(data.user_agent)
+					ref = self.get_or_add_ref(data.referer)
+					id = self.log(data.host, mount, ref, ua, data.bytes)
+
+	def run(self):
+		self.db = self.connect()
+		self.cur = self.db.cursor()
+		file = self.open_file(self.filename)
+		self.read_loop(file)
+
+if __name__ == "__main__":
+	daemon = IceKing('/var/run/iceking.pid')
+	if len(sys.argv) == 2:
+			if 'start' == sys.argv[1]:
+				daemon.start()
+			elif 'stop' == sys.argv[1]:
+				daemon.stop()
+			elif 'restart' == sys.argv[1]:
+				daemon.restart()
+			else:
+				print("Unknown command")
+				sys.exit(2)
+			sys.exit(0)
 	else:
-		id = cur.fetchone()[0]
-		return id
-
-def get_or_add_ref(url):
-	cur.execute('SELECT id FROM referers WHERE url = %s', [url])
-	if cur.rowcount == 0:
-		cur.execute('INSERT INTO referers (url) VALUES (%s) RETURNING id', [url])
-		id = cur.fetchone()[0]
-		if id:
-			db.commit()
-			return id
-		else:
-			raise Exception('cannot add referer: %s' % url)
-	else:
-		id = cur.fetchone()[0]
-		return id
-
-def log(ip, mount, ref, ua, bytes):
-	cur.execute('INSERT INTO log (ip, mount_point_id, referer_id, user_agent_id, bytes) VALUES (%s, %s, %s, %s, %s) RETURNING id', [ip, mount, ref, ua, bytes])
-	id = cur.fetchone()[0]
-	if id:
-		db.commit()
-		return id
-	else:
-		raise Exception('cannot log row: %s %s %s %s %s' % (id, mount, ref, ua, bytes))
-
-
-def open_file(filename):
-	file = open(filename, 'r')
-
-	st_results = os.stat(filename)
-	st_size = st_results[6]
-	file.seek(st_size)
-
-	return file
-
-def read_loop(file):
-	while 1:
-		where = file.tell()
-		line = file.readline()
-		if not line:
-			time.sleep(0.5)
-			file.seek(where)
-		else:
-			data = parse(line)
-			print(line)
-			mount = get_mount_point(data.request)
-			if mount is not None:
-				ua = get_or_add_ua(data.user_agent)
-				ref = get_or_add_ref(data.referer)
-				id = log(data.host, mount, ref, ua, data.bytes)
-
-db = connect()
-cur = db.cursor()
-file = open_file(filename)
-read_loop(file)
+		print("usage: %s start|stop|restart" % sys.argv[0])
+		sys.exit(2)
